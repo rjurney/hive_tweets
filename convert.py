d8da7787f4439661114e143f52fa3d336cd32a5e
Russell Jurney <russell.jurney@gmail.com>
2013-03-11 11:52:07 -0700

    Added convert tweet script

---
+++ convert.py
@@ -0,0 +1,20 @@
+import sys, os, glob
+import json
+
+# Open tweets
+os.chdir("tweets")
+for filename in glob.glob("*.js"):
+  f = open(filename)
+  lines = f.readlines()
+
+  # Chop the first line (its javascript), parse the resulting array of tweets
+  entire = ''
+  for line in lines[1:]:
+    entire += line
+  tweets = json.loads(entire)
+
+  # Now print, one json object per line
+  for tweet in tweets:
+    print json.dumps(tweet)
+  
+    
